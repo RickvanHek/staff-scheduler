bf6e10796b8497f36fe1a75dd6281c36bcd718e7
Rick van Hek <rick.vanhek@tradeling.com>
2022-12-20 11:32:04 +0400

    JWT strategy and guard

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from '../user/user.module';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './local.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [UserModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '600s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
---
+++ src/modules/auth/constants.ts
@@ -0,0 +1,5 @@
+export const jwtConstants = {
+  secret: 'secretKey',
+};
+
+// TODO: dont expose this publicly, use environment variable
